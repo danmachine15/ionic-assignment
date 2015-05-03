c5cf94890bf9ecc4d84a0f1249163dc12c34942d
Daniel Murphy <thedanmachine@outlook.com>
2015-05-03 18:59:16 +0100

    Changed index file

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,10 @@
-angular.module('calorific', ['ionic'])
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('calorific', ['ionic', 'calorific.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -12,3 +18,53 @@ angular.module('calorific', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.status', {
+    url: "/search",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/status.html"
+      }
+    }
+  })
+
+  .state('app.calculator', {
+    url: "/browse",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/calculator.html"
+      }
+    }
+  })
+    .state('app.foodlist', {
+      url: "/playlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/foodlist.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: "/foodlist/:foodlistId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/food.html",
+        controller: 'foodlistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/foodlist');
+});
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,37 @@
 angular.module('calorific.controllers', [])
 
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.personalData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/details.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeDetails = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.enterDetails = function() {
+    console.log('Entering Details', $scope.personalData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeDetails();
+    }, 1000);
+  };
+})
 
 .controller('FoodlistsCtrl', function($scope) {
   $scope.foodlist = [
